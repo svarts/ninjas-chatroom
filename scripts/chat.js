aee949ce5c8fe4407369f737a4e39e029656d057
svartse <killbillbeatrixx@gmail.com>
2023-06-27 00:13:55 +0300

    css changes

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,5 +1,5 @@
 import { db } from "./firebase";
-import { collection } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { collection, Timestamp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 //adding new chat documents
 //setting up a real-time listener to get new chats
 //updating the username
@@ -11,6 +11,23 @@ class Chatroom{
         this.username = username,
         this.chats = collection(db, 'chats');
     }
+    async addChat(message){
+        //format a chat object 
+        const now = new Date();
+        const chat = {
+            message,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+        //save the chat document 
+        const response = await this.chats.add(chat);
+        return response;
+    }
 }
 const chatroom = new Chatroom('gaming', 'shaun');
 console.log(chatroom);
+
+chatroom.addChat('hello everyone')
+    .then(() => console.log('chat added'))
+    .catch(err => console.log(err));
