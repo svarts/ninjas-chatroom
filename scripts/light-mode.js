e3f327f97811fdbaf352383f3b7b0ea48bb00cbb
svartse <killbillbeatrixx@gmail.com>
2023-07-07 17:27:50 +0300

    css animations

--- scripts/light-mode.js
+++ scripts/light-mode.js
@@ -1,9 +1,27 @@
-$( ".change" ).on("click", function() {
+/* $( ".change" ).on("click", function() {
     if( $( "body" ).hasClass( "light" )) {
         $( "body" ).removeClass( "light" );
         $( ".change" ).text( "ON" );
     } else {
         $( "body" ).addClass( "light" );
         $( ".change" ).text( "OFF" );
+    }
+}); */
+$(".change").on("click", function() {
+    if ($("body").hasClass("light")) {
+        $("body").removeClass("light");
+        $(".change").text("ON");
+        $(".container").off("mouseenter mouseleave"); // Remove previous hover event handlers
+    } else {
+        $("body").addClass("light");
+        $(".change").text("OFF");
+        $(".container").hover(
+            function() {
+                $(this).css("box-shadow", "0px 7px 29px 0px rgba(0, 0, 0, 0.2)"); // Change the shadow color on hover
+            },
+            function() {
+                $(this).css("box-shadow", ""); // Reset the shadow color when not hovered
+            },
+        );
     }
 });
